a07f545fef56baaf2655f6011fb9af98b82ac366
peacebefore <peacebefore@gmail.com>
2023-10-12 07:29:57 -0700

    adding template

--- server.py
+++ server.py
@@ -3,16 +3,6 @@ app = Flask(__name__)
 print(__name__)
 
 
-@app.route('/<username>')
-def hello_world(username=None):
-    return render_template('index.html', name=username)
-
-
-@app.route('/about.html')
-def about():
-    return render_template('about.html')
-
-
-@app.route('/blog')
-def blog():
-    return 'These are my thoughts on blogs!'
+@app.route('/')
+def my_home():
+    return render_template('index.html')
