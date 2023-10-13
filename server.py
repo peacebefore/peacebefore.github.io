2708a4c1c0543fdf454ffcfd9a76508bc2beef19
peacebefore <peacebefore@gmail.com>
2023-10-12 20:43:24 -0700

    updating some ui changes and contact section

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 print(__name__)
 
@@ -8,6 +8,15 @@ def my_home():
     return render_template('index.html')
 
 
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
-    return 'form submitted hoorayyy!'
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        return 'yay we did it'
+    else:
+        return 'something went wrong. try again'
